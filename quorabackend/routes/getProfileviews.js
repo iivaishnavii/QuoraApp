7acf10a4e975ac72b43d21bc225c790d6dda9497
Vaishnavi R <vaishnavir@Vaishnavis-MacBook-Pro.local>
2019-05-07 04:56:43 -0700

    Restoring backend quora

--- quorabackend/routes/getProfileviews.js
+++ quorabackend/routes/getProfileviews.js
@@ -5,11 +5,9 @@ var passport = require('passport')
 var requireAuth = passport.authenticate('jwt',{session : false})
 
 
-router.post('/',function(req,res){
-    console.log("in  get profile viewsmmmm")
-    //console.log("req")
-
-   console.log(req.body);
+router.get('/',function(req,res){
+    console.log("in  get top bookmarked answers")
+    
        
         kafka.make_request("getProfileviews",req.body,function(err,result){
             if(err)
@@ -26,77 +24,30 @@ router.post('/',function(req,res){
                     'Content-Type' : 'text/plain'
                 })
              
-                const graph = [];
-                var myMap = new Map();
-                
-                var options = { month: 'long', day: 'numeric' };
-                
-                console.log("after filer");
-                console.log(result);
+        //         const graph = [];
 
-            for (let d in result) {
-
-          let day = (new Date(result[d].dateViewed).toLocaleDateString("en-US", options));
-            if(myMap.has(day)) {
-                myMap.set(day ,myMap.get(day)+ 1);
-            }
-            else {
-                myMap.set(day , 1);
-            }
-
-        
-        }
-
-
-        var date = new Date();
-
-        for (i = 0; i < 29; i++) { 
-            myMap.set( new Date(date.setDate(date.getDate() - 1)).toLocaleDateString("en-US", options), 0);
-          }
-
- 
-          for (let d in result) {
-
-            let day = (new Date(result[d].dateViewed).toLocaleDateString("en-US", options));
-              if(myMap.has(day)) {
-                  myMap.set(day ,myMap.get(day)+ 1);
-              }
-              else {
-                  myMap.set(day , 1);
-              }
-  
-          
-          }
-
-
-
-
-
-
-
-        for (var [key, value] of myMap) {
-
-            let label = key;
-           let y = value;
-
-                const data = {
-            label,y
-          }
-          
-              graph.push(data)
-          }
-       
+        //     for (let d in result) {
+        //   let label = result[d].answer;
+        //   let y = result[d].bookmarks;
     
-        console.log("graph");
-      //  console.log(graph);
+        //   const data = {
+        //     label,y
+        //   }
+    
+        //   graph.push(data)
+        // }
+    
+        // console.log("graph");
+        // console.log(graph);
 
-        let results = {
-            graph : graph
-        }
-console.log(graph)
+        // let results = {
+        //     graph : graph,
+        //     res : result
+        // }
 
 
-                res.end(JSON.stringify(results));
+console.log(result);
+                res.end(JSON.stringify(result));
             }
         })
    // }
